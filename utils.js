32f036b695bdefbb65f2417117020082ba870920
ixaryl <rahimasim15@gmail.com>
2025-07-27 00:09:24 +0400

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,8 @@
+const { customAlphabet } = require('nanoid');
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 7);
+
+function generateShortId() {
+  return nanoid();
+}
+
+module.exports = { generateShortId };
